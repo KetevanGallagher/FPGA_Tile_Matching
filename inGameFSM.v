eb8419ce10ebbc006e69ade9981f6d8c50f1dc87
muffinashes <78507429+muffinashes@users.noreply.github.com>
2025-11-18 21:01:08 -0500

    BIG CHANGES!!!!!!
    add ps2 input, changed the checking systems, made sure previously matched tiles can't be selected, changed the TwoTile to operate on a timer, and other fun things

--- inGameFSM.v
+++ inGameFSM.v
@@ -1,6 +1,12 @@
-module ingameFSM(CLOCK_50, inGameOn, userquit, select1, select2, SW, ledrhldr, hex2hldr, hex3hldr, hex4hldr, hex5hldr, gameOver, currentInGameState);
+module ingameFSM(CLOCK_50, inGameOn, userquit, select1, select2, SW, ps2_key_data, ps2_key_pressed, last_data_received, ledrhldr, hex2hldr, hex3hldr, hex4hldr, hex5hldr, gameOver, currentInGameState);
     input CLOCK_50, inGameOn, userquit, select1, select2;
     input [9:0] SW;
+	//PS2 INPUT STUFF
+	// Internal Wires
+	input [7:0] ps2_key_data;
+	input ps2_key_pressed;
+// Internal Registers
+	input [7:0] last_data_received;
 
     output reg [9:0] ledrhldr;
     output reg [3:0] hex2hldr, hex3hldr, hex4hldr, hex5hldr;
@@ -21,69 +27,96 @@ module ingameFSM(CLOCK_50, inGameOn, userquit, select1, select2, SW, ledrhldr, h
 	assign select1Edge = !select1 && select1Prev;  // falling edge: was high, now low
 	assign select2Edge = !select2 && select2Prev;  // falling edge: was high, now low
 
-    localparam Idle = 3'b000, OneTile = 3'b001, TwoTile = 3'b011, OffGameOver = 3'b100, NotInGame = 3'b101;
+    localparam Idle = 3'b000, OneTile = 3'b001, TwoTile = 3'b011, OffGameOver = 3'b100, NotInGame = 3'b101, Waiting = 3'b010;
     
     //set up each tile, 10 FOR FPGA INTIAL
 	wire [10:0] T_0, T_1, T_2, T_3, T_4, T_5, T_6, T_7, T_8, T_9; 
 	//2-row, 2-col, 6-color, 1-flipped
 	assign T_0 = 11'b00000000010;
-	assign T_1 = 11'b00000000100;
-	assign T_2 = 11'b00000000110;
-	assign T_3 = 11'b00000001000;
-	assign T_4 = 11'b00000000100;
-	assign T_5 = 11'b00000001000;
-	assign T_6 = 11'b00000000110;
-	assign T_7 = 11'b00000000010;
-	assign T_8 = 11'b00000001010;
-	assign T_9 = 11'b00000001010;
-	
+	assign T_1 = 11'b00010000100;
+	assign T_2 = 11'b00100000110;
+	assign T_3 = 11'b00110001000;
+	assign T_4 = 11'b01000000100;
+	assign T_5 = 11'b01010001000;
+	assign T_6 = 11'b01100000110;
+	assign T_7 = 11'b01110000010;
+	assign T_8 = 11'b10000001010;
+	assign T_9 = 11'b10010001010;
+	assign T_10 = 11'b10100000010;
+	assign T_11 = 11'b10110000100;
+	assign T_12 = 11'b11000000110;
+	assign T_13 = 11'b11010001000;
+	assign T_14 = 11'b11100000100;
+	assign T_15 = 11'b11110001000;
+
+//clock stuff
+wire halfsec;
+halfseccounter  tick (CLOCK_50, userquit, halfsec);
+
+reg twosec;
+reg [26:0] counter2;
+//clock_twosec_counter tock (CLOCK_50, userquit, twosec);
+
+
+
     always @ (*)
     begin
         case (currentInGame)
             NotInGame:
                 begin
                     if (inGameOn)
-                        nextInGame = Idle;
+                        nextInGame <= Idle;
                     else
-                        nextInGame = NotInGame;
+                        nextInGame <= NotInGame;
                 end
             Idle:   
                 begin   
                     if (userquit == 1 || !inGameOn) 
-                        nextInGame = NotInGame;
+                        nextInGame <= NotInGame;
                     else if (tileCode1 != 11'b0)
-                        nextInGame = OneTile;
+                        nextInGame <= Waiting;
                     else
-                        nextInGame = Idle;
+                        nextInGame <= Idle;
                 end
+            Waiting: //seeing if adding another state to wait here will stop the issue
+                begin 
+                    if (userquit == 1 || !inGameOn) 
+                        nextInGame <= NotInGame;
+                    else if (halfsec == 1)
+                        nextInGame <= OneTile;
+                    else
+                        nextInGame <= Waiting;
+                end
+
             OneTile: 
                 begin 
                     if (userquit == 1 || !inGameOn) 
-                        nextInGame = NotInGame;
+                        nextInGame <= NotInGame;
                     else if (tileCode2 != 11'b0)
-                        nextInGame = TwoTile;
+                        nextInGame <= TwoTile;
                     else
-                        nextInGame = OneTile;
+                        nextInGame <= OneTile;
                 end
+
             TwoTile:   
                 begin 
                     if (userquit == 1)   
-                        nextInGame = NotInGame;
-                    else if (currentOn == 10'b1111111111)
-                        nextInGame = OffGameOver;
-                    else if (tileCode1 == 11'b0 && tileCode2 == 11'b0)
-                        nextInGame = Idle;
+                        nextInGame <= NotInGame;
+                    else if (gameOver == 1)
+                        nextInGame <= OffGameOver;
+                    else if (twosec)
+                        nextInGame <= Idle;
                     else
-                        nextInGame = TwoTile;
+                        nextInGame <= TwoTile;
                 end
             OffGameOver:
                 begin
-                    if (inGameOn || userquit) 
-                        nextInGame = NotInGame;
+                    if (inGameOn | userquit) 
+                        nextInGame <= NotInGame;
                     else 
-                        nextInGame = OffGameOver;
+                        nextInGame <= OffGameOver;
                 end
-            default: nextInGame = NotInGame;  
+            default: nextInGame <= NotInGame;  
         endcase  
     end
 	 
@@ -113,6 +146,7 @@ module ingameFSM(CLOCK_50, inGameOn, userquit, select1, select2, SW, ledrhldr, h
 		dementiaScore <= 8'b00000000;
 		select1Prev <= 1'b0;
 		select2Prev <= 1'b0;
+		twosec <= 1'b0;
 	end
 
     always @ (posedge CLOCK_50)  
@@ -132,98 +166,112 @@ module ingameFSM(CLOCK_50, inGameOn, userquit, select1, select2, SW, ledrhldr, h
                     tileCode2 <= 11'b00000000000;
                     gameOver <= 1'b0;
                     ledrhldr <= 10'b0000000000;
+		twosec <= 1'b0;
                 end
 
-			Idle:
+		Idle:
 				begin
 					hex2hldr <= 4'b1111;
 					hex3hldr <= 4'b1111;
 					hex4hldr <= dementiaScore[3:0];
 					hex5hldr <= dementiaScore[7:4];
 					gameOver <= 1'b0;
+		twosec <= 1'b0;
 					
 					//show current switches on LEDs combined with matched tiles
 					ledrhldr <= currentOn | SW;
 					
 					//if select1 pressed and a switch is set, select first tile
-		if (select1Edge && |SW)
+		if (ps2_key_pressed)
 		begin
-			if (SW[0]) begin
+			if (ps2_key_data == 8'h16 && !currentOn[0]) begin
 				tileCode1 <= T_0;
 				nextCurrentOn1 <= currentOn | 10'b0000000001;
-			end else if (SW[1]) begin
+			end else if (ps2_key_data == 8'h1e && !currentOn[1]) begin
 				tileCode1 <= T_1;
 				nextCurrentOn1 <= currentOn | 10'b0000000010;
-			end else if (SW[2]) begin
+			end else if (ps2_key_data == 8'h26 && !currentOn[2]) begin
 				tileCode1 <= T_2;
 				nextCurrentOn1 <= currentOn | 10'b0000000100;
-			end else if (SW[3]) begin
+			end else if (ps2_key_data == 8'h25 && !currentOn[3]) begin
 				tileCode1 <= T_3;
 				nextCurrentOn1 <= currentOn | 10'b0000001000;
-			end else if (SW[4]) begin
+			end else if (ps2_key_data == 8'h15 && !currentOn[4]) begin
 				tileCode1 <= T_4;
 				nextCurrentOn1 <= currentOn | 10'b0000010000;
-			end else if (SW[5]) begin
+			end else if (ps2_key_data == 8'h1d && !currentOn[5]) begin
 				tileCode1 <= T_5;
 				nextCurrentOn1 <= currentOn | 10'b0000100000;
-			end else if (SW[6]) begin
+			end else if (ps2_key_data == 8'h24 && !currentOn[6]) begin
 				tileCode1 <= T_6;
 				nextCurrentOn1 <= currentOn | 10'b0001000000;
-			end else if (SW[7]) begin
+			end else if (ps2_key_data == 8'h2d && !currentOn[7]) begin
 				tileCode1 <= T_7;
 				nextCurrentOn1 <= currentOn | 10'b0010000000;
-			end else if (SW[8]) begin
+			end else if (ps2_key_data == 8'h1c && !currentOn[8]) begin
 				tileCode1 <= T_8;
 				nextCurrentOn1 <= currentOn | 10'b0100000000;
-			end else if (SW[9]) begin
+			end else if (ps2_key_data == 8'h1b && !currentOn[9]) begin
 				tileCode1 <= T_9;
 				nextCurrentOn1 <= currentOn | 10'b1000000000;
 			end
 		end
 				end
 
-			OneTile:
+            Waiting:
+                begin
+                    ledrhldr <= nextCurrentOn1;
+					hex3hldr <= tileCode1[5:1];
+                    hex2hldr <= 4'b1111;
+                    hex4hldr <= dementiaScore[3:0];
+                    hex5hldr <= dementiaScore[7:4];
+                    gameOver <= 1'b0; 
+		twosec <= 1'b0;
+                end
+
+		OneTile:
 				begin
 					hex2hldr <= 4'b1111;
 					hex3hldr <= tileCode1[5:1];
 					hex4hldr <= dementiaScore[3:0];
 					hex5hldr <= dementiaScore[7:4];
 					gameOver <= 1'b0;
+		twosec <= 1'b0;
 					
 					//show first selected tile on LEDs
 					ledrhldr <= nextCurrentOn1;
 					
 					//if select2 pressed and a switch is set, select second tile
-		if (select2Edge && |SW)
+		if (ps2_key_pressed && (ps2_key_data != last_data_received))
 		begin
-			if (SW[0]) begin
+			if (ps2_key_data == 8'h16 && T_0 != tileCode1 && !currentOn[0]) begin
 				tileCode2 <= T_0;
 				nextCurrentOn2 <= nextCurrentOn1 | 10'b0000000001;
-			end else if (SW[1]) begin
+			end else if (ps2_key_data == 8'h1e && T_1 != tileCode1 && !currentOn[1]) begin
 				tileCode2 <= T_1;
 				nextCurrentOn2 <= nextCurrentOn1 | 10'b0000000010;
-			end else if (SW[2]) begin
+			end else if (ps2_key_data == 8'h26 && T_2 != tileCode1 && !currentOn[2]) begin
 				tileCode2 <= T_2;
 				nextCurrentOn2 <= nextCurrentOn1 | 10'b0000000100;
-			end else if (SW[3]) begin
+			end else if (ps2_key_data == 8'h25 && T_3 != tileCode1 && !currentOn[3]) begin
 				tileCode2 <= T_3;
 				nextCurrentOn2 <= nextCurrentOn1 | 10'b0000001000;
-			end else if (SW[4]) begin
+			end else if (ps2_key_data == 8'h15 && T_4 != tileCode1 && !currentOn[4]) begin
 				tileCode2 <= T_4;
 				nextCurrentOn2 <= nextCurrentOn1 | 10'b0000010000;
-			end else if (SW[5]) begin
+			end else if (ps2_key_data == 8'h1d && T_5 != tileCode1 && !currentOn[5]) begin
 				tileCode2 <= T_5;
 				nextCurrentOn2 <= nextCurrentOn1 | 10'b0000100000;
-			end else if (SW[6]) begin
+			end else if (ps2_key_data == 8'h24 && T_6 != tileCode1 && !currentOn[6]) begin
 				tileCode2 <= T_6;
 				nextCurrentOn2 <= nextCurrentOn1 | 10'b0001000000;
-			end else if (SW[7]) begin
+			end else if (ps2_key_data == 8'h2d && T_7 != tileCode1 && !currentOn[7]) begin
 				tileCode2 <= T_7;
 				nextCurrentOn2 <= nextCurrentOn1 | 10'b0010000000;
-			end else if (SW[8]) begin
+			end else if (ps2_key_data == 8'h1c && T_8 != tileCode1 && !currentOn[8]) begin
 				tileCode2 <= T_8;
 				nextCurrentOn2 <= nextCurrentOn1 | 10'b0100000000;
-			end else if (SW[9]) begin
+			end else if (ps2_key_data == 8'h1b && T_9 != tileCode1 && !currentOn[9]) begin
 				tileCode2 <= T_9;
 				nextCurrentOn2 <= nextCurrentOn1 | 10'b1000000000;
 			end
@@ -232,53 +280,60 @@ module ingameFSM(CLOCK_50, inGameOn, userquit, select1, select2, SW, ledrhldr, h
 
 			TwoTile:
 				begin
-					hex2hldr <= tileCode2[5:1];
-					hex3hldr <= tileCode1[5:1];
-					hex4hldr <= dementiaScore[3:0];
-					hex5hldr <= dementiaScore[7:4];
-					
-					//show both selected tiles on LEDs
-					ledrhldr <= nextCurrentOn2;
-					
-					//if select1 pressed, compare tiles
-					if (select1Edge)
-					begin
-						dementiaScore <= dementiaScore + 1;
+				hex2hldr <= tileCode2[5:1];
+				hex3hldr <= tileCode1[5:1];
+				hex4hldr <= dementiaScore[3:0];
+				hex5hldr <= dementiaScore[7:4];
+
+				if (counter2 == 27'b101111101011110000100000000) //if the counter has reached 0.5 seconds 
+			//for modelsim change to this line instead, does every 5 cycles as a halfsec 
+			// if (counter == 26'b00000000000000000000000101) 
+					begin 
+					counter2 <= 27'b0; 
+
+				//if select1 pressed, compare tiles
+					dementiaScore <= dementiaScore + 1;
 						
-						if (tileCode1[5:1] == tileCode2[5:1])
+					if (tileCode1[5:1] == tileCode2[5:1])
 						begin
-							//LEDs stay on
-							currentOn <= nextCurrentOn2;
-							ledrhldr <= nextCurrentOn2;
+						//LEDs stay on
+						currentOn <= nextCurrentOn2;
+						ledrhldr <= nextCurrentOn2;
 							
-							//check if all tiles matched
-							if (nextCurrentOn2 == 10'b1111111111)
-								gameOver <= 1'b1;
-							else
-								gameOver <= 1'b0;
-						end
+						//check if all tiles matched
+						if (nextCurrentOn2 == 10'b1111111111)
+							gameOver <= 1'b1;
 						else
-						begin
-							//LEDs turn off
-							ledrhldr <= currentOn;
 							gameOver <= 1'b0;
 						end
-						
-						//clear LEDS, turn off hex displays
-						tileCode1 <= 11'b0;
-						tileCode2 <= 11'b0;
-						nextCurrentOn1 <= 10'b0;
-						nextCurrentOn2 <= 10'b0;
-					end
 					else
-					begin
-						gameOver <= (currentOn == 10'b1111111111) ? 1'b1 : 1'b0;
+						begin
+						//LEDs turn off
+						ledrhldr <= currentOn;
+						gameOver <= 1'b0;
+						end
+						
+					//clear LEDS, turn off hex displays
+					tileCode1 <= 11'b0;
+					tileCode2 <= 11'b0;
+					nextCurrentOn1 <= 10'b0;
+					nextCurrentOn2 <= 10'b0;
+
+					twosec <= ~twosec; 
+					end 
+				else 
+					begin 
+					twosec <= 0; 
+					counter2 <= counter2 + 1; 
+					//show both selected tiles on LEDs
+					ledrhldr <= nextCurrentOn2;
 					end
+					
 				end
 
             OffGameOver:
                 begin
-                    ledrhldr <= 10'b0;
+                    ledrhldr <= currentOn;
                     hex3hldr <= 4'b1111;
                     hex2hldr <= 4'b1111;
                     hex4hldr <= dementiaScore[3:0];
@@ -298,7 +353,7 @@ module ingameFSM(CLOCK_50, inGameOn, userquit, select1, select2, SW, ledrhldr, h
     end
 endmodule
 
-module clock_twosec_counter(Clock, clear, pulse);
+module clock_twosec_counter (Clock, clear, pulse);
 	input Clock, clear;
 	reg [26:0]counter;
 	output reg pulse;
@@ -324,3 +379,26 @@ module clock_twosec_counter(Clock, clear, pulse);
 			end
 	end
 endmodule
+
+module halfseccounter (Clock, clear, pulse);
+	input Clock, clear;
+	reg [26:0] counter;
+	output reg pulse;
+	always@(posedge Clock)
+	begin
+		if (counter == 26'b01011111010111100001000000) //if the counter has reached 0.5 seconds 
+
+		//for modelsim change to this line instead, does every 5 cycles as a halfsec 
+		// if (counter == 26'b00000000000000000000000101) 
+			begin 
+			counter <= 26'b0; 
+			pulse <= ~pulse; 
+			end 
+		else 
+			begin 
+			pulse <= 0; 
+			counter <= counter + 1; 
+			end
+	end
+endmodule
+
--- testbench.v
+++ testbench.v
@@ -1,14 +1,23 @@
 `timescale 1ns / 1ps
 
 module testbench ( );
-
 	parameter CLOCK_PERIOD = 10;
 
     reg [9:0] SW;
     reg [3:0] KEY;
     reg CLOCK_50;
+	wire PS2_CLK;
+	wire PS2_DAT;
     wire [9:0] LEDR;
     wire [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+
+//additional signals for ps2
+	reg [7:0] ps2_key_data;
+	reg ps2_key_pressed;
+    
+    // TODO: Remove these debug signals before final implementation
+    wire [3:0] gameModeState;
+    wire [2:0] inGameState;
 
 	initial begin
         CLOCK_50 <= 1'b0;
@@ -17,26 +26,135 @@ module testbench ( );
 	begin : Clock_Generator
 		#((CLOCK_PERIOD) / 2) CLOCK_50 <= ~CLOCK_50;
 	end
+
+	initial begin
+        SW  = 10'b0;
+        KEY = 4'b1111;
+
+        // ============================================
+        // TEST CASE 1: Start game
+        // ============================================
+        #50 ps2_key_pressed <= 1'b1; ps2_key_data = 8'h5a;      // Press enter key to start
+        #10 ps2_key_pressed <= 1'b0;
+        #100; // wait for state transition
+
+        // ============================================
+        // TEST CASE 2: Non-matching tiles (SW[0] and SW[1])
+        // SW[0] = color 1, SW[1] = color 2 (no match)
+        // ============================================
+        #50 ps2_key_pressed <= 1'b1; ps2_key_data = 8'h16;      // Press tile 0
+        #10 ps2_key_pressed <= 1'b0;
+        #100; // wait for state transition
+        
+        #50 ps2_key_pressed <= 1'b1; ps2_key_data = 8'h1e;      // Press tile 1
+        #10 ps2_key_pressed <= 1'b0;
+        #100; // wait for state transition
+        
+        #200; // wait for comparison
+
+        // ============================================
+        // TEST CASE 3: Matching tiles (SW[0] and SW[7])
+        // Both have color 1 (should match)
+        // ============================================
+        #50 ps2_key_pressed <= 1'b1; ps2_key_data = 8'h16;      // Press tile 0
+        #10 ps2_key_pressed <= 1'b0;
+        #100; // wait for state transition
+        
+        #50 ps2_key_pressed <= 1'b1; ps2_key_data = 8'h2d;      // Press tile 7
+        #10 ps2_key_pressed <= 1'b0;
+        #100; // wait for state transition
+        
+        #200; // wait for comparison
+
+        // ============================================
+        // TEST CASE 4: Another matching pair (SW[1] and SW[4])
+        // Both have color 2 (should match)
+        // ============================================
+        #50 ps2_key_pressed <= 1'b1; ps2_key_data = 8'h1e;      // Press tile 1
+        #10 ps2_key_pressed <= 1'b0;
+        #100; // wait for state transition
+        
+        #50 ps2_key_pressed <= 1'b1; ps2_key_data = 8'h15;      // Press tile 4
+        #10 ps2_key_pressed <= 1'b0;
+        #100; // wait for state transition
+        
+        #200; // wait for comparison
+
+        // ============================================
+        // TEST CASE 5: Non-matching tiles (SW[2] and SW[3])
+        // SW[2] = color 3, SW[3] = color 4 (no match)
+        // ============================================
+        #50 ps2_key_pressed <= 1'b1; ps2_key_data = 8'h26;      // Press tile 2
+        #10 ps2_key_pressed <= 1'b0;
+        #100; // wait for state transition
+
+        #50 ps2_key_pressed <= 1'b1; ps2_key_data = 8'h25;      // Press tile 3
+        #10 ps2_key_pressed <= 1'b0;
+        #100; // wait for state transition
+        
+        #200; // wait for comparison
+
+        // ============================================
+        // TEST CASE 6: Matching pair (SW[2] and SW[6])
+        // Both have color 3 (should match)
+        // ============================================
+        #50 ps2_key_pressed <= 1'b1; ps2_key_data = 8'h26;      // Press tile 2
+        #10 ps2_key_pressed <= 1'b0;
+        #100; // wait for state transition
+        
+        #50 ps2_key_pressed <= 1'b1; ps2_key_data = 8'h24;      // Press tile 6
+        #10 ps2_key_pressed <= 1'b0;
+        #100; // wait for state transition
+        
+        #200; // wait for comparison
+
+        // ============================================
+        // TEST CASE 7: Matching pair (SW[3] and SW[5])
+        // Both have color 4 (should match)
+        // ============================================
+        #50 ps2_key_pressed <= 1'b1; ps2_key_data = 8'h25;      // Press tile 3
+        #10 ps2_key_pressed <= 1'b0;
+        #100; // wait for state transition
+        
+        #50 ps2_key_pressed <= 1'b1; ps2_key_data = 8'h1d;      // Press tile 5
+        #10 ps2_key_pressed <= 1'b0;
+        #100; // wait for state transition
+        
+        #200; // wait for comparison
+
+        // ============================================
+        // TEST CASE 8: Matching pair (SW[8] and SW[9])
+        // Both have color 5 (should match)
+        // ============================================
+        #50 ps2_key_pressed <= 1'b1; ps2_key_data = 8'h1c;      // Press tile 8
+        #10 ps2_key_pressed <= 1'b0;
+        #100; // wait for state transition
+        
+        #50 ps2_key_pressed <= 1'b1; ps2_key_data = 8'h1b;      // Press tile 9
+        #10 ps2_key_pressed <= 1'b0;
+        #100; // wait for state transition
+        
+        #200; // wait for comparison
+
+        // ============================================
+        // TEST CASE 9: Test reset (KEY[0])
+        // ============================================
+        #200 KEY[0] <= 1'b0;  // Press KEY[0] to reset
+        #200 KEY[0] <= 1'b1;  // Release
+        #200; // wait for reset
+
+        // ============================================
+        // TEST CASE 10: Restart game and test edge case
+        // ============================================
+        #200 ps2_key_pressed <= 1'b1; ps2_key_data = 8'h5a;      // Press enter key to start
+        #70 ps2_key_pressed <= 1'b0;
+        #200; // wait for state transition
+
+	end // initial
+
+	tilegame U1 (SW, KEY, CLOCK_50, PS2_CLK, PS2_DAT, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
 	
-	initial begin
-        KEY[0] <= 1'b0;
-        #10 KEY[0] <= 1'b1;
-	end // initial
-
-    // In the setup below we assume that the half_sec_enable signal coming
-    // from the half-second clock is asserted every 3 cycles of CLOCK_50. Of
-    // course, in the real circuit the half-second clock is asserted every
-    // 25M cycles. The setup below produces the Morse code for A (.-) followed
-    // by the Morse code for B (-...).
-	initial begin
-        SW <= 9'b0; KEY[1] = 1; KEY[0] = 1;// not pressed;
-        #10 KEY[1] <= 0; KEY[0] <= 1;// pressed to eswitch game mode
-        #10 KEY[1] <= 0; KEY[0] <= 0; //pressed reset
-        #10 KEY[1] <= [1]; //started game again
-        #10 SW <= 9'b000000001;
-        #20 SW <= 9'b000000101;
-        #30 KEY[0] <= 0; // not pressed
-	end // initial
-	tilegame U1 (SW, KEY, CLOCK_50, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
-
+	// Remove these if not debugging
+	assign gameModeState = U1.gameModeState;
+	assign inGameState = U1.inGameState;
 endmodule
