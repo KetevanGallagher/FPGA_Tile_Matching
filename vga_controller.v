a373706fd8c61274de9328ffefae668cdc036ece
Ketevan Gallagher <KetevanGallagher@gmail.com>
2025-11-24 18:41:04 -0500

    vga controller fixed- pixel clock and horizontal and vertical sync

--- vga_controller.v
+++ vga_controller.v
@@ -21,14 +21,14 @@ module vga_controller(vga_clock, resetn, xCoord, yCoord, VGA_HS, VGA_VS, visible
 	 * than are actually present on the screen. This is necessary to generate the vertical
      * and horizontal syncronization signals.  */
 	parameter C_VERT_NUM_PIXELS  = 11'd480;
-	parameter C_VERT_SYNC_START  = 11'd493;
-	parameter C_VERT_SYNC_END    = 11'd494; //(C_VERT_SYNC_START + 2 - 1); 
-	parameter C_VERT_TOTAL_COUNT = 11'd525;
+	parameter C_VERT_SYNC_START  = 11'd490; //vresolution plus vfrontporch
+	parameter C_VERT_SYNC_END    = 11'd492; //vsyncstart plus vpulse; 
+	parameter C_VERT_TOTAL_COUNT = 11'd525; // vsync end plus vbackporch
 
 	parameter C_HORZ_NUM_PIXELS  = 11'd640;
-	parameter C_HORZ_SYNC_START  = 11'd659;
-	parameter C_HORZ_SYNC_END    = 11'd754; //(C_HORZ_SYNC_START + 96 - 1); 
-	parameter C_HORZ_TOTAL_COUNT = 11'd800;	
+	parameter C_HORZ_SYNC_START  = 11'd656; // hresolution plus hfrontporch
+	parameter C_HORZ_SYNC_END    = 11'd752; //hsyncstart plus hpulse; 
+	parameter C_HORZ_TOTAL_COUNT = 11'd800; // h sync end plus hbackporch
 		
 	/*****************************************************************************/
 	/* Declare inputs and outputs.                                               */
@@ -58,14 +58,25 @@ module vga_controller(vga_clock, resetn, xCoord, yCoord, VGA_HS, VGA_VS, visible
 	reg [9:0] y;	
 	/* Inputs to the converter. */
 	
+	reg pixelClkInternal;
+	
 	/*****************************************************************************/
 	/* Controller implementation.                                                */
 	/*****************************************************************************/
 
 	assign vcc =1'b1;
 	
+	//added: get 25mhz clock
+	always @(posedge vga_clock)
+	begin
+		if (!resetn)
+			pixelClkInternal <= 1'b0;
+		else
+			pixelClkInternal <= ~pixelClkInternal;
+	end
+	
 	/* A counter to scan through a horizontal line. */
-	always @(posedge vga_clock or negedge resetn)
+	always @(posedge pixelClkInternal or negedge resetn)
 	begin
 		if (!resetn)
 			xCounter <= 10'd0;
@@ -79,7 +90,7 @@ module vga_controller(vga_clock, resetn, xCoord, yCoord, VGA_HS, VGA_VS, visible
 	assign xCounter_clear = (xCounter == (C_HORZ_TOTAL_COUNT-1));
 
 	/* A counter to scan vertically, indicating the row currently being drawn. */
-	always @(posedge vga_clock or negedge resetn)
+	always @(posedge pixelClkInternal or negedge resetn)
 	begin
 		if (!resetn)
 			yCounter <= 10'd0;
@@ -104,7 +115,7 @@ module vga_controller(vga_clock, resetn, xCoord, yCoord, VGA_HS, VGA_VS, visible
 
 
 	/* Generate the vertical and horizontal synchronization pulses. */
-	always @(posedge vga_clock)
+	always @(posedge pixelClkInternal)
 	begin
 		//- Sync Generator (ACTIVE LOW)
 		VGA_HS1 <= ~((xCounter >= C_HORZ_SYNC_START) && (xCounter <= C_HORZ_SYNC_END));
@@ -123,7 +134,7 @@ module vga_controller(vga_clock, resetn, xCoord, yCoord, VGA_HS, VGA_VS, visible
 	assign VGA_SYNC_N = vcc;
 	
 	/* Generate the VGA clock signal. */
-	assign pixelClk = vga_clock;
+	assign pixelClk = pixelClkInternal;
 	
 
 
