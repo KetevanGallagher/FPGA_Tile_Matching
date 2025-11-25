8199720fce2b71a6808b8638b8c7dfc8a05aba90
muffinashes <78507429+muffinashes@users.noreply.github.com>
2025-11-24 20:46:09 -0500

    new deci

--- FPGAdisplay.v
+++ FPGAdisplay.v
@@ -1,7 +1,7 @@
-module FPGAdisplay(userquit, ingameOn, gameOver, hex0hldr, dementiaScore, ledrhldr, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
+module FPGAdisplay(userquit, ingameOn, gameOver, hex0hldr, hex4hldr, hex5hldr, ledrhldr, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
 input userquit, ingameOn, gameOver;
 input [3:0] hex0hldr;
-input [7:0] dementiaScore;
+input [3:0] hex4hldr, hex5hldr;
 input [9:0] ledrhldr;
 
 output [9:0] LEDR;
@@ -28,48 +28,45 @@ assign LEDR = ledrhldr;
 endmodule
 
 //module for changing the dementia score into proper decimal
-module decimal_conversion(bi, deci);
-    input [7:0] bi;
-    output reg [7:0] deci;
+module decimal_conversion(bi4, bi5 deci);
+	input [3:0] bi4, bi5;
+	output reg [7:0] deci4, deci5;
 
-    always @(*) 
-    begin
-        case(bi)
-            8'b00000000: deci = 8'b00000000; //0
-            8'b00000001: deci = 8'b00000001; //1
-            8'b00000010: deci = 8'b00000010; //2
-            8'b00000011: deci = 8'b00000011; //3
-            8'b00000100: deci = 8'b00000100; //4
-            8'b00000101: deci = 8'b00000101; //5
-            8'b00000110: deci = 8'b00000110; //6
-            8'b00000111: deci = 8'b00000111; //7
-            8'b00001000: deci = 8'b00001000; //8
-            8'b00001001: deci = 8'b00001001; //9
-            8'b00001010: deci = 8'b00010000; //10
-            8'b00001011: deci = 8'b00010001; //11
-            8'b00001100: deci = 8'b00010010; //12
-            8'b00001101: deci = 8'b00010011; //13
-            8'b00001110: deci = 8'b00010100; //14
-            8'b00001111: deci = 8'b00010101; //15
-            8'b00010001: deci = 8'b00010110; //16
-            8'b00010010: deci = 8'b00010111; //17
-            8'b00010011: deci = 8'b00011000; //18
-            8'b00010100: deci = 8'b00011001; //19
-            8'b00010101: deci = 8'b00100000; //20
-            8'b00010110: deci = 8'b00100001; //21
-            8'b00010111: deci = 8'b00100010; //22
-            8'b00011000: deci = 8'b00100011; //23
-            8'b00011001: deci = 8'b00100100; //24
-            8'b00011010: deci = 8'b00100101; //25
-            8'b00011011: deci = 8'b00100110; //26
-            8'b00011100: deci = 8'b00100111; //27
-            8'b00011101: deci = 8'b00101000; //28
-            8'b00011110: deci = 8'b00101001; //29
-            8'b00011111: deci = 8'b00110000; //30
-            8'b00100000: deci = 8'b00110001; //31
-            8'b00100001: deci = 8'b00110010; //32
-            default: deci = 8'b1111111;
-        endcase
+	always @(*) 
+	begin
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0000; deci4 = 4'b0000; end //0
+	if (bi5 = 4'b0000 && bi4 = 4'b0001) begin deci5 = 4'b0000; deci4 = 4'b0001; end //1
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0000; deci4 = 4'b0010; end //2
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0000; deci4 = 4'b0011; end //3
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0000; deci4 = 4'b0100; end //4
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0000; deci4 = 4'b0101; end //5
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0000; deci4 = 4'b0110; end //6
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0000; deci4 = 4'b0111; end //7
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0000; deci4 = 4'b1000; end //8
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0000; deci4 = 4'b1001; end //9
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0001; deci4 = 4'b0000; end //10
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0001; deci4 = 4'b0001; end //11
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0001; deci4 = 4'b0010; end //12
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0001; deci4 = 4'b0011; end //13
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0001; deci4 = 4'b0100; end //14
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0001; deci4 = 4'b0101; end //15
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0001; deci4 = 4'b0110; end //16
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0001; deci4 = 4'b0111; end //17
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0001; deci4 = 4'b1000; end //18
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0001; deci4 = 4'b1001; end //19
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0010; deci4 = 4'b0000; end //20
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0010; deci4 = 4'b0001; end //21
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0010; deci4 = 4'b0010; end //22
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0010; deci4 = 4'b0011; end //23
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0010; deci4 = 4'b0100; end //24
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0010; deci4 = 4'b0101; end //25
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0010; deci4 = 4'b0110; end //26
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0010; deci4 = 4'b0111; end //27
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0010; deci4 = 4'b1000; end //28
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0010; deci4 = 4'b1001; end //29
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0011; deci4 = 4'b0000; end //30
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0011; deci4 = 4'b0001; end //31
+	if (bi5 = 4'b0000 && bi4 = 4'b0000) begin deci5 = 4'b0011; deci4 = 4'b0010; end //32
     end
 endmodule
 
